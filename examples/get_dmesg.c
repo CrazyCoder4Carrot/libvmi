10d501d1ba1d6d0932fee402cfad6cf342b21b90
ytliu <mctrain016@gmail.com>
2013-08-07 22:55:11 -0400

    add get_dmesg vmi tool

---
+++ examples/get_dmesg.c
@@ -0,0 +1,81 @@
+/* The LibVMI Library is an introspection library that simplifies access to 
+ * memory in a target virtual machine or in a file containing a dump of 
+ * a system's physical memory.  LibVMI is based on the XenAccess Library.
+ *
+ * Copyright 2011 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government
+ * retains certain rights in this software.
+ *
+ * Author: Yutao Liu (mctrain016@gmail.com)
+ *
+ * This file is part of LibVMI.
+ *
+ * LibVMI is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * LibVMI is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with LibVMI.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libvmi/libvmi.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    vmi_instance_t vmi;
+    addr_t log_offset = 0;
+    vmi_pid_t pid = 0;
+    unsigned long tasks_offset, pid_offset, name_offset;
+
+    /* this is the VM or file that we are looking at */
+    if (argc != 2) {
+        printf("Usage: %s <vmname>\n", argv[0]);
+        return 1;
+    } // if
+
+    char *name = argv[1];
+
+    /* initialize the libvmi library */
+    if (vmi_init(&vmi, VMI_AUTO | VMI_INIT_COMPLETE, name) == VMI_FAILURE) {
+        printf("Failed to init LibVMI library.\n");
+        return 1;
+    }
+
+    /* init the offset values */
+    if (VMI_OS_LINUX == vmi_get_ostype(vmi)) {
+        tasks_offset = vmi_get_offset(vmi, "linux_tasks");
+        name_offset = vmi_get_offset(vmi, "linux_name");
+        pid_offset = vmi_get_offset(vmi, "linux_pid");
+    }
+
+    /* pause the vm for consistent memory access */
+    if (vmi_pause_vm(vmi) != VMI_SUCCESS) {
+        printf("Failed to pause VM\n");
+        goto error_exit;
+    } // if
+
+	vmi_read_addr_ksym(vmi, "log_buf", &log_offset);
+	printf("log_buf: 0x%lx\n", log_offset);
+	char *dmesg = vmi_read_str_va(vmi, log_offset, 0);
+	printf("dmesg: %s\n", dmesg);
+
+    error_exit:
+    /* resume the vm */
+    vmi_resume_vm(vmi);
+
+    /* cleanup any memory associated with the LibVMI instance */
+    vmi_destroy(vmi);
+
+    return 0;
+}
